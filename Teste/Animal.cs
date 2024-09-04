d2a52d0357ea6635b4c3a46cbb5bb35104867498
mdmassa <djsheepwolfbr@gmail.com>
2024-09-04 14:32:01 -0300

    Commit inciial

---
+++ Teste/Animal.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Animal {
+    public string nome;
+    public double peso;
+
+    public Animal(string nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public virtual void Locomover() {
+        Console.WriteLine($"{nome} se locomove de forma padrão.");
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Teste;
+
+class Program {
+    static void Main(string[] args) {
+        Animal a = new Animal("Gato", 5.74);
+
+        a.Locomover();
+    
+    }
+}
